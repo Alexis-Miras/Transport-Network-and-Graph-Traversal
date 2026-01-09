f5c4aaa51b130ccdad85fa3b845e4a173aaa00b9
Alexis-Miras <alexisof77@gmail.com>
2026-01-10 00:58:42 +0100

    implementation

---
+++ include/hash.h
@@ -0,0 +1,19 @@
+#ifndef HASH_H
+#define HASH_H
+
+#define TABLE_SIZE 1000
+
+struct HashNode {
+    char *name;
+    int id;
+    struct HashNode *next;
+};
+typedef struct HashNode* HashMap;
+
+void init_hash();
+void insert_hash(char *, int);
+int search_hash(char *);
+void free_hash();
+void print_hash_table();
+
+#endif
---
+++ src/hash.c
@@ -0,0 +1,66 @@
+#include "../include/hash.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+HashMap hash_table[TABLE_SIZE];
+
+void init_hash(void) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        hash_table[i] = NULL;
+    }
+}
+
+static unsigned int hash(char *str) {
+    unsigned int h = 0;
+    while (*str) {
+        h = (h * 31 + *str) % TABLE_SIZE;
+        str++;
+    }
+    return h;
+}
+
+void insert_hash(char *name, int id) {
+    unsigned int idx = hash(name);
+    HashMap node = malloc(sizeof(struct HashNode));
+    node->name = strdup(name);
+    node->id = id;
+    node->next = hash_table[idx];
+    hash_table[idx] = node;
+}
+
+int search_hash(char *name) {
+    unsigned int idx = hash(name);
+    HashMap node = hash_table[idx];
+    while (node) {
+        if (!strcmp(node->name, name)) return node->id;
+        node = node->next;
+    }
+    return -1;
+}
+
+void free_hash() {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        struct HashNode *node = hash_table[i];
+        while (node) {
+            struct HashNode *tmp = node;
+            node = node->next;
+            free(tmp->name);
+            free(tmp);
+        }
+    }
+}
+
+void print_hash_table() {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        HashMap node = hash_table[i];
+        if (node) {
+            printf("Bucket %d:\n", i);
+            while (node) {
+                printf("  name='%s', id=%d\n", node->name, node->id);
+                node = node->next;
+            }
+        }
+    }
+}
